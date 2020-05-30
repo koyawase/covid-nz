e4fb53144730ba91bf8055b1a1676bd04884d707
koyawase <waseemkoya@gmail.com>
2020-05-30 22:39:43 +1200

    create title

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
   StackedHorizontalBarChart
 } from "./components";
 
+import logo from "./logo.svg";
+
 import { fetchData } from "./api";
 
 import "./App.css";
@@ -25,6 +27,11 @@ class App extends React.Component {
     const { data } = this.state;
     return (
       <div className="container">
+        <div className="title">
+          <div className="title-covid"><p className="title-c">C</p> <img src={logo} className="App-logo" /><p className="title-vid">VID-19</p></div>
+
+          <p className="title-tracker">Tracker</p>
+        </div>
         <CaseDisplay data={data} />
         <LineChartTracker />
       </div>
